76945eda9dd53738025d4b5d4fb67b53e3b48a85
Jerome Dane <jerome.dane@gmail.com>
2022-07-19 13:48:28 -0400

    Update userscript.js

--- userscript.js
+++ userscript.js
@@ -11,12 +11,12 @@
 // Source https://github.com/JeromeDane/slack-auto-show-unreads-userscript/blob/master/userscript.js
 
 (function() {
-  var lastActivity = Date.now(),
-      interval = 10000, // check for new unread messages every interval milliseconds
-      activityTimeout = 30000 // don't load new unreads messages if there has been activity in the last activityTimeout milliseconds and there are already unreads shown
+  var lastActivity = Date.now()
+  const interval = 5000, // check for new unread messages every interval milliseconds
+        activityTimeout = 30000 // don't load new unreads messages if there has been activity in the last activityTimeout milliseconds and there are already unreads shown
   document.body.onmousemove = function() { lastActivity = Date.now() }
   function getRefreshButton() {
-    var buttons = document.querySelectorAll('.p-ia__view_header .c-icon--sync')
+    const buttons = document.querySelectorAll('.p-ia__view_header .c-icon--sync')
     return buttons && buttons[0]
   }
   function getShowMoreLinks() {
@@ -26,18 +26,24 @@
     var button = elemIfNotHidden(document.querySelector('.p-unreads_view__empty button.c-button--primary'))
     return button && !button.className.match(/undo/i) && button
   }
+  const hideEmptyMessage = () => {
+    const unreadsEmptyMessage = document.querySelector('.p-unreads_view__empty__message')
+    if(unreadsEmptyMessage) unreadsEmptyMessage.style.display = 'none'
+  }
   function checkForUnreads() {
-    var newMessagesButton = getNewMessagesButton()
+    // hideEmptyMessage()
+    const newMessagesButton = getNewMessagesButton()
     if(newMessagesButton) simulateClick(newMessagesButton)
     if(Date.now() - lastActivity > activityTimeout) {
       var refreshButton = getRefreshButton()
       if(refreshButton) simulateClick(refreshButton)
+      else document.location.reload()
       getShowMoreLinks().forEach(simulateClick)
     }
     setTimeout(checkForUnreads, interval)
   }
   function simulateClick(element) { // https://github.com/JeromeDane/simulate-click-js
-    var clickEvent = document.createEvent("MouseEvents")
+    const clickEvent = document.createEvent("MouseEvents")
     clickEvent.initEvent("mousedown", true, true)
     element.dispatchEvent(clickEvent)
     clickEvent = document.createEvent("MouseEvents")
@@ -49,4 +55,5 @@
   }
   function elemIfNotHidden(el) { return el && el.offsetParent != null && el }
   checkForUnreads()
+  // setTimeout(hideEmptyMessage, 1000)
 })()
